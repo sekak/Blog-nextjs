97d7ecb4dac8b7551831f00cf0cb084c8aa87bde
Ahmed Sekkak <asekkak@c2r2p8.1337.ma>
2023-12-20 11:20:22 +0100

    next-mch

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,9 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+ 
+ 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE,
   authDomain: "template-914ab.firebaseapp.com",
@@ -16,26 +14,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
-
- 
-
- // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-//  // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA7V0bMl3mw-ELpsN0SRQvFMv6DgMlO1kI",
-//   authDomain: "blog-83765.firebaseapp.com",
-//   projectId: "blog-83765",
-//   storageBucket: "blog-83765.appspot.com",
-//   messagingSenderId: "756834342422",
-//   appId: "1:756834342422:web:38d30b46ee8dee4ab08737",
-//   measurementId: "G-2BRRND0KMJ"
-// };
-
-// // Initialize Firebase
-// export const app = initializeApp(firebaseConfig);
  
